c9c73e88a4b8c3c7692dd832ac635f0a25c141f7
Ola Pohorecka Mrożek <aleksandra.pohorecka@chargify.com>
2020-09-12 12:00:07 +0200

    Add event handlers

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -4,11 +4,11 @@ function Calculator() {
     return (
         <form>
             <div>
-                <input type="number" placeholder="Amount"/>
+                <input type="number" placeholder="Amount" onChange={(event) => console.log(event.target.value)}/>
             </div>
             <div>
                 <span>From:</span>
-                <select>
+                <select onChange={(event) => console.log(event.target.value)}>
                     <option value="USD">USD</option>
                     <option value="PLN">PLN</option>
                     <option value="GBP">GBP</option>
@@ -17,12 +17,15 @@ function Calculator() {
             </div>
             <div>
                 <span>To:</span>
-                <select>
+                <select onChange={(event) => console.log(event.target.value)}>
                     <option value="USD">USD</option>
                     <option value="PLN">PLN</option>
                     <option value="GBP">GBP</option>
                     <option value="EUR">EUR</option>
                 </select>
+            </div>
+            <div>
+                <span>Result:</span>
             </div>
         </form>
         )
