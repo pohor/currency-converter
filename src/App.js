1b05df941b40944f9e9d32b0e3da2a760f5ee0e2
Ola Pohorecka Mrożek <aleksandra.pohorecka@chargify.com>
2020-09-12 13:57:15 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Link,
+    Route
+} from 'react-router-dom';
+
 import './App.css';
 // import { Button } from "./components/Button";
 import Calculator from "./pages/Calculator";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
-        <Calculator/>
+        <Router>
+            <ul>
+                <li><Link to="">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            <Switch>
+                <Route path="/" component={Calculator} exact/>
+                <Route path="/about" component={About}/>
+                <Route path="/contact">
+                    <Contact/>
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
